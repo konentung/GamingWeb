3329136a217e97db52a0cb14098b432a5259674c
konentung <84655730+konentung@users.noreply.github.com>
2025-03-31 23:15:35 +0800

    Test

--- GamingWeb/settings.py
+++ GamingWeb/settings.py
@@ -29,9 +29,11 @@ if os.path.isfile(dotenv_path):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['gameweb.azurewebsites.net']
+ALLOWED_HOSTS = ['gameweb.azurewebsites.net', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://gameweb.azurewebsites.net']
 
 AUTH_USER_MODEL = 'accounts.Account'
 
